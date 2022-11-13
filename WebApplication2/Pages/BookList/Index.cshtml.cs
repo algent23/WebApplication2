2420c325fdc1d65360a1c662f40e17358368ab77
algent <algent@ALGENT-PC>
2022-11-13 16:38:43 +0100

    updated

--- WebApplication2/Pages/BookList/Index.cshtml.cs
+++ WebApplication2/Pages/BookList/Index.cshtml.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using System.Collections;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using WebApplication2.Model;
 
@@ -14,7 +15,7 @@ namespace WebApplication2.Pages.BookList
         {
             this.db = db;
         }
-        public IEnumerable<Book> Books { get; set; }
+        public IEnumerable <Book> Books { get; set; }
         public async Task OnGet()
         {
             Books=await this.db.Book.ToListAsync();
